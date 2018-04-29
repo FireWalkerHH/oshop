05fa5e668bd58fd743c4c396954359a23f8a718c
FireWalkerHH <thomas.franzek@gmail.com>
2018-04-29 10:44:28 +0200

    Changed stringLiterals to constant

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -3,22 +3,23 @@ import { AngularFireDatabase } from 'angularfire2/database';
 
 @Injectable()
 export class ProductService {
+  url = '/products';
 
   constructor(private db: AngularFireDatabase) { }
 
   create(product) {
-    return this.db.list('/products').push(product);
+    return this.db.list(this.url).push(product);
   }
 
   getAll() {
-    return this.db.list('/products');
+    return this.db.list(this.url);
   }
 
   get(productId) {
-    return this.db.object('/products/' + productId);
+    return this.db.object(this.url + '/' + productId);
   }
 
   update(productId, product) {
-    return this.db.object('/products/' + productId).update(product);
+    return this.db.object(this.url + '/' + productId).update(product);
   }
 }
